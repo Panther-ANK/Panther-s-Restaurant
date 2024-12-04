06863ae66f0ad7fda9c70264961c9504ca2c86f9
Panther-ANK <153408749+Panther-ANK@users.noreply.github.com>
2024-12-04 12:02:19 +0530

    `

---
+++ sitedata/js/scrollanimation.js
@@ -0,0 +1,15 @@
+const obs = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if(entry.isIntersecting){
+        entry.target.classList.add('show-sec')
+      }
+      
+      else{
+        entry.target.classList.remove('show-sec')
+      }
+    })
+  })
+
+  const hidden = document.querySelectorAll('.hidden-sec')
+  hidden.forEach((e) => obs.observe(e))
